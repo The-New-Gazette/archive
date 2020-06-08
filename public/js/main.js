e68b9b400b1f3af7274d5333bab6a831d71a4db7
Jared Wasdin <jared.wasdin@gmail.com>
2020-06-08 16:38:20 -0500

    params test 2

--- public/js/main.js
+++ public/js/main.js
@@ -15,15 +15,13 @@ for (i = 0; i < acc.length; i++) {
 }
 
 // Referral
-var getParams = function (url) {
-    var params = {};
-    var parser = document.createElement('a');
-    parser.href = url;
-    var query = parser.search.substring(1);
-    var vars = query.split('&');
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=');
-        params[pair[0]] = decodeURIComponent(pair[1]);
-    }
-    return params;
-};
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
