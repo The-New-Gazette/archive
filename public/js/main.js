bd673a338f3d41bc1b0549d56292e529e0d714de
Jared Wasdin <jared.wasdin@gmail.com>
2020-06-11 14:53:50 -0500

    referral use uuid

--- public/js/main.js
+++ public/js/main.js
@@ -25,3 +25,14 @@ function getQueryVariable(variable)
    }
    return(false);
 }
+
+// Generate UUID
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'tng-yxxxxxxx-4xxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
