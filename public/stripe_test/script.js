b1f6344b294d3c4287d8ee83d2651d5b8ea2fabf
Jared Wasdin <jared.wasdin@gmail.com>
2021-02-16 21:50:12 -0600

    add stripe test

---
+++ public/stripe_test/script.js
@@ -0,0 +1,38 @@
+// Create a Checkout Session with the selected plan ID
+var createCheckoutSession = function(priceId) {
+  return fetch("/create-checkout-session", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      priceId: priceId
+    })
+  }).then(handleFetchResult);
+};
+
+// Handle any errors returned from Checkout
+var handleResult = function(result) {
+  if (result.error) {
+    showErrorMessage(result.error.message);
+  }
+};
+
+var showErrorMessage = function(message) {
+  var errorEl = document.getElementById("error-message")
+  errorEl.textContent = message;
+  errorEl.style.display = "block";
+};
+
+document
+  .getElementById("checkout")
+  .addEventListener("click", function(evt) {
+    createCheckoutSession("price_1IKwfiHZTUfzXfXGT5UetMg8").then(function(data) {
+      // Call Stripe.js method to redirect to the new Checkout page
+      stripe
+        .redirectToCheckout({
+          sessionId: data.sessionId
+        })
+        .then(handleResult);
+    });
+  });
